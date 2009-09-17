9631871fcdfec87d8513ed1850cb98e0efdbb7de
Michael Ernst <mernst@cs.washington.edu>
2009-09-16 20:02:40 -0700

    Adjust bootclasspath to include Java 7 version of ElementType

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,6 +6,10 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%annotation-file-utilities.jar
+:: Contains Java 7 version of java.lang.annotation.ElementType.
+set JAVAC_JAR=%~d0
+set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
+set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
 
-java -cp "%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotations.io.classfile.ClassFileReader %*
+java "-Xbootclasspath/p:%ANNOTATION_FILE_UTILS%;%JAVAC_JAR%" -cp "%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotations.io.classfile.ClassFileReader %*
 
