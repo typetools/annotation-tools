7b3849c86b2f14e19ca19f665961b47c7e28fc64
Michael Ernst <mernst@cs.washington.edu>
2009-10-19 20:28:02 -0700

    Remove out-of-date version of example program from HTML documentation. Make the actual .java version be run as part of the tests.

--- scene-lib/src/annotations/tests/Example.java
+++ scene-lib/src/annotations/tests/Example.java
@@ -20,65 +20,59 @@ import annotations.io.*;
  * </pre>
  **/
 public class Example {
-    public static void main(String /*@ReadOnly*/ [] args) {
-        AScene scene;
+  public static void main(String /*@ReadOnly*/ [] args) {
+    AScene scene;
 
-        System.out.println("Reading in " + args[0]);
-        try {
-            scene = new AScene();
-            IndexFileParser.parse(new LineNumberReader(new FileReader(args[0])), scene);
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-            return;
-        } catch (ParseException e) {
-            e.printStackTrace(System.err);
-            return;
-        }
-
-        System.out.println("Processing class " + args[1]);
-        // Get a handle on the class
-        AClass clazz1 = scene.classes.get(args[1]);
-        if (clazz1 == null) {
-            System.out
-                    .println("That class is never mentioned in the annotation file!");
-            return;
-        }
-        AClass clazz =
-                (AClass) clazz1;
-
-        for (Map.Entry<String, AMethod> me : clazz.methods
-                .entrySet()) {
-            AMethod method = me.getValue();
-
-            Annotation rro = method.receiver.lookup("ReadOnly");
-            if (rro == null)
-                System.out.println("Method " + me.getKey()
-                        + " might modify the receiver");
-            else
-                System.out.println("Method " + me.getKey()
-                        + " must not modify the receiver");
-
-            ATypeElement param1 = method.parameters.vivify(0);
-            Annotation p1nn = param1.lookup("NonNull");
-            if (p1nn == null) {
-                System.out.println("Annotating first parameter of "
-                        + me.getKey() + " nonnull");
-
-                param1.tlAnnotationsHere.add(Annotations.aNonNull);
-            }
-        }
-
-        System.out.println("Writing out " + args[2]);
-        try {
-            IndexFileWriter.write(scene, new FileWriter(args[2]));
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-            return;
-        } catch (DefException e) {
-            e.printStackTrace(System.err);
-            return;
-        }
-
-        System.out.println("Success.");
+    System.out.println("Reading in " + args[0]);
+    try {
+      scene = new AScene();
+      IndexFileParser.parseFile(args[0], scene);
+    } catch (IOException e) {
+      e.printStackTrace(System.err);
+      return;
     }
+
+    System.out.println("Processing class " + args[1]);
+    // Get a handle on the class
+    AClass clazz1 = scene.classes.get(args[1]);
+    if (clazz1 == null) {
+      System.out.println("Class " + args[1] + " is not mentioned in annotation file " + args[0]);
+      return;
+    }
+    AClass clazz = (AClass) clazz1;
+
+    for (Map.Entry<String, AMethod> me : clazz.methods.entrySet()) {
+      AMethod method = me.getValue();
+
+      Annotation rro = method.receiver.lookup("ReadOnly");
+      if (rro == null)
+        System.out.println("Method " + me.getKey()
+            + " might modify the receiver");
+      else
+        System.out.println("Method " + me.getKey()
+            + " must not modify the receiver");
+
+      ATypeElement param1 = method.parameters.vivify(0);
+      Annotation p1nn = param1.lookup("NonNull");
+      if (p1nn == null) {
+        System.out.println("Annotating first parameter of "
+            + me.getKey() + " nonnull");
+
+        param1.tlAnnotationsHere.add(Annotations.aNonNull);
+      }
+    }
+
+    System.out.println("Writing out " + args[2]);
+    try {
+      IndexFileWriter.write(scene, new FileWriter(args[2]));
+    } catch (IOException e) {
+      e.printStackTrace(System.err);
+      return;
+    } catch (DefException e) {
+      e.printStackTrace(System.err);
+      return;
+    }
+
+    System.out.println("Success.");
+  }
 }
