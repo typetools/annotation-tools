8bd6c61414cbae8628ef95ed7d4e0b92dfe4d825
Michael Ernst <mernst@cs.washington.edu>
2010-09-19 20:54:39 -0700

    Change 'Sun' to 'Oracle'

--- annotation-file-utilities/scripts/insert-annotations-to-source.bat
+++ annotation-file-utilities/scripts/insert-annotations-to-source.bat
@@ -5,7 +5,7 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%..\annotation-file-utilities.jar
-:: Contains Java 7 version of java.lang.annotation.ElementType.
+:: Contains new version of java.lang.annotation.ElementType.
 set JAVAC_JAR=%~d0
 set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
 set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
