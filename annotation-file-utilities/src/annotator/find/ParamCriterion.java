6c089f09e27c112d1ac736df061b075f39a445a0
Werner Dietl <wdietl@gmail.com>
2010-10-02 15:40:28 -0700

    Remove dead code/imports.

--- annotation-file-utilities/src/annotator/find/ParamCriterion.java
+++ annotation-file-utilities/src/annotator/find/ParamCriterion.java
@@ -1,14 +1,6 @@
 package annotator.find;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import annotations.el.InnerTypeLocation;
-
-import com.sun.source.tree.ArrayTypeTree;
-import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.ParameterizedTypeTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
@@ -16,7 +8,6 @@ import com.sun.source.util.TreePath;
 public class ParamCriterion implements Criterion {
 
   private String methodName;
-  private Criterion parentCriterion;
   private Integer paramPos;
 
   public ParamCriterion(String methodName, Integer pos) {
