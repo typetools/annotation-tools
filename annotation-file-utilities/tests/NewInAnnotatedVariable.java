1c0af97ab2cf0f4ea862fa6160e3a69225ab739e
Michael Ernst <mernst@cs.washington.edu>
2010-12-13 11:44:14 -0800

    Comment type annotations, to permit compiling tests with a Java 6 compiler

--- annotation-file-utilities/tests/NewInAnnotatedVariable.java
+++ annotation-file-utilities/tests/NewInAnnotatedVariable.java
@@ -3,8 +3,8 @@
 
 public class NewInAnnotatedVariable {
   Number b1 = new Integer(0);
-  @NonNull Object b2 = new @Nullable Double(1);
-  @NonNull Runnable b3 = new @NonNull Thread();
+  @NonNull Object b2 = new /*@Nullable*/ Double(1);
+  @NonNull Runnable b3 = new /*@NonNull*/ Thread();
   ThreadLocal[] b4 = new InheritableThreadLocal[3];
 }
 
