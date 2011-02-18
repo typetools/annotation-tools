43537d0329c5624337f2ec040d3f6bd5c5d70555
wdietl <wdietl@localhost>
2011-02-18 14:30:31 -0800

    Ensure that the path is within the correct method. This prevents problems with anonymous inner classes. Eclipse wants some more @Override annotations.

--- annotation-file-utilities/src/annotator/find/NewCriterion.java
+++ annotation-file-utilities/src/annotator/find/NewCriterion.java
@@ -12,10 +12,13 @@ import com.sun.source.util.TreePath;
 public class NewCriterion implements Criterion {
 
   private String methodName;
+  private Criterion inMethodCriterion;
+
   private RelativeLocation loc;
 
   public NewCriterion(String methodName, RelativeLocation loc) {
     this.methodName = methodName.substring(0, methodName.lastIndexOf(")") + 1);
+    this.inMethodCriterion = Criteria.inMethod(methodName);
     this.loc = loc;
   }
 
@@ -35,9 +38,10 @@ public class NewCriterion implements Criterion {
 
     Tree leaf = path.getLeaf();
 
-    if (leaf.getKind() == Tree.Kind.NEW_CLASS
-        || leaf.getKind() == Tree.Kind.NEW_ARRAY) {
-      int indexInSource = NewScanner.indexOfNewTree(path, leaf, methodName);
+    if (inMethodCriterion.isSatisfiedBy(path) &&
+            (leaf.getKind() == Tree.Kind.NEW_CLASS
+                    || leaf.getKind() == Tree.Kind.NEW_ARRAY)) {
+      int indexInSource = NewScanner.indexOfNewTree(path, leaf);
       // System.out.printf("indexInSource=%d%n", indexInSource);
       boolean b;
       if (loc.isBytecodeOffset()) {
@@ -52,10 +56,12 @@ public class NewCriterion implements Criterion {
     }
   }
 
+  @Override
   public Kind getKind() {
     return Kind.NEW;
   }
 
+  @Override
   public String toString() {
     return "NewCriterion in method: " + methodName + " at location " + loc;
   }
