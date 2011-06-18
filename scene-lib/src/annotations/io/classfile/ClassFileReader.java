9bddd855a98cab25aaa04242d6ef5fa74a368d67
Werner Dietl <wdietl@gmail.com>
2011-06-18 14:58:53 -0700

    Update files for the 3.2 release.

--- scene-lib/src/annotations/io/classfile/ClassFileReader.java
+++ scene-lib/src/annotations/io/classfile/ClassFileReader.java
@@ -18,7 +18,7 @@ import annotations.io.IndexFileWriter;
 public class ClassFileReader {
 
   public static final String INDEX_UTILS_VERSION
-    = "Annotation File Utilities v3.1";
+    = "Annotation File Utilities v3.2";
 
   @Option("-h print usage information and exit")
   public static boolean help = false;
