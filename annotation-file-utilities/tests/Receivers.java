67ef4360e4d0b2c90124f1185357989682d62359
Eric Spishak <espishak@cs.washington.edu>
2012-12-03 23:05:08 -0800

    First commit fixing Issue 21. This correctly inserts a receiver annotation with the receiver parameter syntax. This does not yet support inserting multiple receiver annotations to the same place or inserting annotations on the type parameters of the receiver.

---
+++ annotation-file-utilities/tests/Receivers.java
@@ -0,0 +1,38 @@
+package annotator.tests;
+
+public class Receivers {
+    public void m() {}
+
+    public void m(int i) {}
+
+    public void m(@Anno() String s) {}
+}
+
+class Receivers2 {
+    public void m(Receivers2 this) {}
+
+    public void m(Receivers2 this, int i) {}
+}
+
+class Receivers3<K, V> {
+    public void m() {}
+
+    public void m(int i) {}
+}
+
+class Receivers4<K, V> {
+    public void m(Receivers4<K, V> this) {}
+
+    public void m(Receivers4<K, V> this, int i) {}
+}
+
+interface Receivers5 {
+    public void m();
+}
+
+enum Receivers6 {
+    TEST;
+    public void m() {}
+}
+
+@interface Anno {}
