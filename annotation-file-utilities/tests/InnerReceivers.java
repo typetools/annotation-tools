b502970e9ddde46afee614d0737f8066e20db33e
Eric Spishak <espishak@cs.washington.edu>
2013-01-31 23:48:26 -0800

    Use the full type name (Outer.Inner1.Inner2) for source code receiver insertions in inner classes.

--- annotation-file-utilities/tests/InnerReceivers.java
+++ annotation-file-utilities/tests/InnerReceivers.java
@@ -10,4 +10,22 @@ public class InnerReceivers {
             void m(Inner this) {}
         }
     };
+
+    public InnerReceivers() {}
+
+    void m(InnerReceivers this) {}
+
+    class Inner1<Y, Z> {
+
+        public Inner1() {}
+
+        void m(InnerReceivers.Inner1<Y, Z> this) {}
+
+        class Inner2 {
+
+            public Inner2() {}
+
+            void m(InnerReceivers.Inner1<Y, Z>.Inner2 this) {}
+        }
+    }
 }
