0d983abecf56f196bde8542ab26977d29b713215
Eric Spishak <espishak@cs.washington.edu>
2013-02-12 17:36:00 -0800

    Add separate Kind for Static Init criterion to prevent an illegal cast.

--- annotation-file-utilities/src/annotator/find/Criterion.java
+++ annotation-file-utilities/src/annotator/find/Criterion.java
@@ -41,6 +41,7 @@ public interface Criterion {
         CLASS_BOUND,
         IN_PACKAGE,
         AST_PATH,
+        IN_STATIC_INIT,
         /*
          * This constant is never used. What is the difference to IN_CLASS?
          * Is one for anywhere within a class and this one only for the
