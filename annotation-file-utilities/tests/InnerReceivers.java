ba9056ee462a783436a9e9fb6e5183be62c443f5
Eric Spishak <espishak@cs.washington.edu>
2013-02-13 21:21:53 -0800

    Insert the annotation on the correct type for receiver insertions.

--- annotation-file-utilities/tests/InnerReceivers.java
+++ annotation-file-utilities/tests/InnerReceivers.java
@@ -1,3 +1,5 @@
+package annotator.tests;
+
 public class InnerReceivers {
 
     InnerReceivers i = new InnerReceivers() {
@@ -15,17 +17,46 @@ public class InnerReceivers {
 
     void m(InnerReceivers this) {}
 
+    void m2(annotator.tests.InnerReceivers this) {}
+
     class Inner1<Y, Z> {
 
         public Inner1() {}
 
         void m(InnerReceivers.Inner1<Y, Z> this) {}
 
+        void m2(annotator.tests.InnerReceivers.Inner1<Y, Z> this) {}
+
         class Inner2 {
 
             public Inner2() {}
 
             void m(InnerReceivers.Inner1<Y, Z>.Inner2 this) {}
+        }
+    }
+
+    static class StaticInner1 {
+
+        public StaticInner1() {}
+
+        void m(InnerReceivers.StaticInner1 this) {}
+
+        void m2(annotator.tests.InnerReceivers.StaticInner1 this) {}
+    }
+}
+
+class Outer<K> {
+    static class StaticInner2 {
+
+        public StaticInner2() {}
+
+        void m(Outer.StaticInner2 this) {}
+
+        void m2(annotator.tests.Outer.StaticInner2 this) {}
+
+        static class StaticInner3 {
+
+            public StaticInner3() {}
         }
     }
 }
