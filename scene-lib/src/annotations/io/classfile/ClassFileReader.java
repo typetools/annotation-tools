8dec68e07621bf69e9d71a006a2422c0331622f5
jonathangburke <jonathangburke@gmail.com>
2013-05-02 00:44:04 -0700

    Release 3.5.3

--- scene-lib/src/annotations/io/classfile/ClassFileReader.java
+++ scene-lib/src/annotations/io/classfile/ClassFileReader.java
@@ -19,7 +19,7 @@ import annotations.io.IndexFileWriter;
 public class ClassFileReader {
 
   public static final String INDEX_UTILS_VERSION
-    = "Annotation File Utilities v3.5.2";
+    = "Annotation File Utilities v3.5.3";
 
   @Option("-h print usage information and exit")
   public static boolean help = false;
