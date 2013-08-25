70ff6e46ffaaac4938786276ee2f6570a448020f
Michael Ernst <mernst@cs.washington.edu>
2013-08-25 10:04:18 -0700

    Hack to work around Jenkins build problem with "-target 1.8"

--- annotation-file-utilities/src/annotator/Source.java
+++ annotation-file-utilities/src/annotator/Source.java
@@ -59,7 +59,9 @@ public final class Source {
             .getJavaFileObjectsFromStrings(Collections.singletonList(src));
 
         // Compiler options.
-        final String[] stringOpts = new String[] { "-g"};
+        // -Xlint:-options is a hack to get around Jenkins build problem:
+        // "target value 1.8 is obsolete and will be removed in a future release"
+        final String[] stringOpts = new String[] { "-g", "-Xlint:-options" };
           //TODO: figure out if these options are necessary? "-source", "1.6x"
         List<String> optsList = Arrays.asList(stringOpts);
 
