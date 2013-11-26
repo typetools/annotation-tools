0a49483461880503eafd96a58b7cd6c87b22c575
Dan Brown <dbro@cs.washington.edu>
2013-11-26 00:52:49 -0800

    Adds tests for cast, new, and constructor inside an enum declaration.

--- annotation-file-utilities/tests/NonClass.java
+++ annotation-file-utilities/tests/NonClass.java
@@ -2,4 +2,8 @@ public class NonClass {
   interface A { void m(Object p); }
   enum B { ONE; void m() { Object l; } }
   @interface C { String value() default "Ha!"; }
+  enum E {
+    D((String) new String());
+    E(String s) { }
+  }
 }
