da0b1c8a6fd7a76cce1a0ad420d4a98cff2f65c2
Dan Brown <dbro@cs.washington.edu>
2014-03-20 11:30:27 -0700

    Puts annotations in comments so Eclipse doesn't complain.

--- scene-lib/src/annotations/tests/classfile/foo/G.java
+++ scene-lib/src/annotations/tests/classfile/foo/G.java
@@ -8,6 +8,6 @@ public @interface G {
   int fieldA();
   String fieldB();
   boolean[] fieldC();
-  @D(fieldA=1, fieldB="value from annotation @G", fieldC={3,2}) int fieldD();
-  @D(fieldA=2,fieldB="value from annotation @G",fieldC={3,2}) int fieldE();
+  /*@D(fieldA=1, fieldB="value from annotation @G", fieldC={3,2})*/ int fieldD();
+  /*@D(fieldA=2,fieldB="value from annotation @G",fieldC={3,2})*/ int fieldE();
 }
