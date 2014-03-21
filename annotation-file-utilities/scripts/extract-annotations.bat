10957ee8ff0e0b4a125f753322e1bff657bb4dbd
Dan Brown <dbro@cs.washington.edu>
2014-03-21 10:19:16 -0700

    Corrects path to AFU jar. Fixes Issue #76.

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -5,7 +5,7 @@
 
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
-set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%annotation-file-utilities.jar
+set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities.jar
 :: Contains new version of java.lang.annotation.ElementType.
 set JAVAC_JAR=%~d0
 set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
