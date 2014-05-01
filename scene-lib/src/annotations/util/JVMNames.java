e975fd2a976763d6547091161ca4919c558c365b
David McArthur <mcarthur@cs.washington.edu>
2014-05-01 11:35:55 -0700

    Removed hacky string replacement and instead treated arrays specially.

--- scene-lib/src/annotations/util/JVMNames.java
+++ scene-lib/src/annotations/util/JVMNames.java
@@ -1,16 +1,15 @@
 package annotations.util;
 
 import com.sun.source.tree.MethodTree;
-import com.sun.tools.javac.code.Symbol;
-import com.sun.tools.javac.code.Symbol.VarSymbol;
+import com.sun.tools.javac.code.Type;
 import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
 import plume.UtilMDE;
 
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.VariableElement;
-import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
-import java.util.regex.Pattern;
 
 /**
  * Class to generate class formatted names from Trees.
@@ -19,53 +18,52 @@ import java.util.regex.Pattern;
  */
 public class JVMNames {
 
-
     /**
-     * Hacky way of getting the byte code signature for a method.
+     * Converts a MethodTree into a jvml format method signature.
      * There is probably an API to do this, but I couldn't find it.
      *
-     * @param methodTree
-     * @return
+     * @param methodTree the tree to convert
+     * @return a String signature of methodTree in jvml format
      */
     public static String getJVMMethodName(MethodTree methodTree) {
         ExecutableElement methodElement = ((JCMethodDecl) methodTree).sym;
         StringBuilder builder = new StringBuilder();
         builder.append(methodTree.getName());
         builder.append("(");
+
         for (VariableElement ve : methodElement.getParameters()) {
-            builder.append(UtilMDE.binaryNameToFieldDescriptor(stripAnnotations(((VarSymbol) ve).asType().tsym.flatName().toString())));
+            builder.append(typeToJvmlString((Type) ve.asType()));
         }
+
         builder.append(")");
 
         TypeMirror returnType = methodElement.getReturnType();
-        final String returnTypeStr;
-        if (methodElement.getReturnType() instanceof DeclaredType) {
-            returnTypeStr = stripAnnotations(((Symbol)((DeclaredType)returnType).asElement()).asType().tsym.flatName().toString());
-        } else {
-            returnTypeStr = stripAnnotations(methodElement.getReturnType().toString());
-        }
+        String returnTypeStr = typeToJvmlString((Type)returnType);
 
         // Special case void since UtilMDE doesn't handle it.
         if ("void".equals(returnTypeStr)) {
             builder.append("V");
         } else {
-            builder.append(UtilMDE.binaryNameToFieldDescriptor(returnTypeStr));
+            builder.append(returnTypeStr);
         }
         return builder.toString();
     }
 
-    // Types with anntations end up looking like:
-    // (@sparta/checkers/quals/Source({sparta/checkers/quals/FlowPermission/CAMERA}) :: byte)
-    // We just want the base type.
-    private static final Pattern REPLACE_PATTERN = Pattern.compile("(\\(@.+?:: ([^\\)]+)\\))");
-
     /**
-     * Remove the annotations from the input method signature.
-     * @param inputType
-     * @return
+     * Create a JVML string for a type.
+     * Uses {@link UtilMDE#binaryNameToFieldDescriptor(String)}
+     *
+     * Array strings are built by recursively converting the component type.
+     *
+     * @param type the Type to convert to JVML
+     * @return The JVML representation of type
      */
-    private static String stripAnnotations(String inputType) {
-        return REPLACE_PATTERN.matcher(inputType).replaceAll("$2");
+    private static String typeToJvmlString(Type type) {
+        if (type.getKind() == TypeKind.ARRAY) {
+            return "[" + typeToJvmlString((Type) ((ArrayType) type).getComponentType());
+        } else {
+            return UtilMDE.binaryNameToFieldDescriptor(type.tsym.flatName().toString());
+        }
     }
 }
 
