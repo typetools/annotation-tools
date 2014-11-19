9d72ffba3e0c4e235aa3e71131dd17ed8640d91c
Dan Brown <dbro@cs.washington.edu>
2014-11-19 13:17:38 -0800

    Adds root of source AST as data member.

--- scene-lib/src/annotations/io/ASTRecord.java
+++ scene-lib/src/annotations/io/ASTRecord.java
@@ -3,15 +3,13 @@ package annotations.io;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-import annotations.util.JVMNames;
-
 import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
 import com.sun.source.tree.MethodTree;
 import com.sun.source.tree.Tree;
 import com.sun.source.tree.Tree.Kind;
 import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
-import com.sun.tools.javac.tree.JCTree.JCClassDecl;
 
 /**
  * Structure bundling an {@link ASTPath} with information about its
@@ -21,6 +19,11 @@ import com.sun.tools.javac.tree.JCTree.JCClassDecl;
  * @author dbro
  */
 public class ASTRecord implements Comparable<ASTRecord> {
+  /**
+   * The AST to which this {@code ASTRecord} pertains.
+   */
+  public final CompilationUnitTree ast;
+
   /**
    * Name of the enclosing class declaration.
    */
@@ -41,14 +44,15 @@ public class ASTRecord implements Comparable<ASTRecord> {
    */
   public final ASTPath astPath;
 
-  public ASTRecord(String className, String methodName, String varName,
-      ASTPath astPath) {
+  public ASTRecord(CompilationUnitTree ast, String className,
+      String methodName, String varName, ASTPath astPath) {
+    this.ast = ast;
     this.className = className;
     this.methodName = methodName;
     this.varName = varName;
     // FIXME: ensure path is canonical
     if (varName != null) {
-      // TODO
+      // TODO?
     } else if (methodName != null) {
       int n = astPath.size();
       if (n > 0 && astPath.get(0).getTreeKind() != Tree.Kind.METHOD
@@ -62,79 +66,13 @@ public class ASTRecord implements Comparable<ASTRecord> {
     this.astPath = astPath;
   }
 
-  static ASTRecord fromPaths(TreePath treePath, ASTPath astPath) {
-    ClassTree clazz = null;
-    MethodTree method = null;
-    VariableTree field = null;
-
-    for (Tree node : treePath) {
-      Tree.Kind kind = node.getKind();
-      switch (kind) {
-      case CLASS:
-      case INTERFACE:
-      case ENUM:
-      case ANNOTATION_TYPE:
-        clazz = (ClassTree) node;
-        return assemble(astPath, clazz, method, field);
-      case METHOD:
-        method = (MethodTree) node;
-        field = null;
-        break;
-      case VARIABLE:
-        field = (VariableTree) node;
-        break;
-      default:
-        method = null;
-        field = null;
-      }
-    }
-
-    return null;
-  }
-
-  private static ASTRecord assemble(ASTPath astPath,
-      ClassTree classNode, MethodTree methodNode, VariableTree fieldNode) {
-    if (classNode == null) { return null; }
-
-    String className = ((JCClassDecl) classNode).sym.flatname.toString();
-    if (methodNode == null) {
-      if (fieldNode != null) {
-        return new ASTRecord(className, null,
-            fieldNode.getName().toString(), astPath);
-      }
-      return new ASTRecord(className, null, null, astPath);
-    }
-
-    String methodName = JVMNames.getJVMMethodName(methodNode);
-    if (fieldNode == null) {
-      return new ASTRecord(className, methodName, null, astPath);
-    }
-
-    String fieldName = fieldNode.getName().toString();
-    VariableTree recv = methodNode.getReceiverParameter();
-    if (recv != null && recv.getName().toString().equals(fieldName)) {
-      return new ASTRecord(className, methodName,
-          Integer.toString(-1), astPath);
-    }
-
-    int i = 0;
-    for (VariableTree param : methodNode.getParameters()) {
-      if (param.getName().toString().equals(fieldName)) {
-        return new ASTRecord(className, methodName, Integer.toString(i),
-            astPath);
-      }
-      i++;
-    }
-    return null;
-  }
-
   public ASTRecord newArrayLevel(int depth) {
-    return new ASTRecord(className, methodName, varName,
+    return new ASTRecord(ast, className, methodName, varName,
         astPath.extendNewArray(depth));
   }
 
   public ASTRecord replacePath(ASTPath newPath) {
-    return new ASTRecord(className, methodName, varName, newPath);
+    return new ASTRecord(ast, className, methodName, varName, newPath);
   }
 
   @Override
@@ -148,21 +86,27 @@ public class ASTRecord implements Comparable<ASTRecord> {
 
   @Override
   public int compareTo(ASTRecord rec) {
-    int d = className == null
-        ? rec.className == null ? 0 : -1
-        : rec.className == null ? 1 : className.compareTo(rec.className);
+    int d = ast == null
+        ? rec.ast == null ? 0 : -1
+        : rec.ast == null ? 1 : Integer
+            .compare(ast.hashCode(), rec.ast.hashCode());
     if (d == 0) {
-      d = methodName == null
-          ? rec.methodName == null ? 0 : -1
-          : rec.methodName == null ? 1 : methodName.compareTo(rec.methodName);
+      d = className == null
+          ? rec.className == null ? 0 : -1
+          : rec.className == null ? 1 : className.compareTo(rec.className);
       if (d == 0) {
-        d = varName == null
-            ? rec.varName == null ? 0 : -1
-            : rec.varName == null ? 1 : varName.compareTo(rec.varName);
+        d = methodName == null
+            ? rec.methodName == null ? 0 : -1
+            : rec.methodName == null ? 1 : methodName.compareTo(rec.methodName);
         if (d == 0) {
-          d = astPath == null
-              ? rec.astPath == null ? 0 : -1
-              : rec.astPath == null ? 1 : astPath.compareTo(rec.astPath);
+          d = varName == null
+              ? rec.varName == null ? 0 : -1
+              : rec.varName == null ? 1 : varName.compareTo(rec.varName);
+          if (d == 0) {
+            d = astPath == null
+                ? rec.astPath == null ? 0 : -1
+                : rec.astPath == null ? 1 : astPath.compareTo(rec.astPath);
+          }
         }
       }
     }
@@ -171,13 +115,15 @@ public class ASTRecord implements Comparable<ASTRecord> {
 
   @Override
   public int hashCode() {
-    return astPath.hashCode()
+    return ast.hashCode()
         ^ (className == null ? 0
-            : Integer.rotateRight(className.hashCode(), 4))
+            : Integer.rotateRight(className.hashCode(), 3))
         ^ (methodName == null ? 0
-            : Integer.rotateRight(methodName.hashCode(), 8))
+            : Integer.rotateRight(methodName.hashCode(), 6))
         ^ (varName == null ? 0
-            : Integer.rotateRight(varName.hashCode(), 12));
+            : Integer.rotateRight(varName.hashCode(), 9))
+        ^ (astPath == null ? 0
+            : Integer.rotateRight(astPath.hashCode(), 12));
   }
 
   /**
@@ -235,7 +181,8 @@ public class ASTRecord implements Comparable<ASTRecord> {
   }
 
   public ASTRecord extend(ASTPath.ASTEntry entry) {
-    return new ASTRecord(className, methodName, varName, astPath.extend(entry));
+    return new ASTRecord(ast, className, methodName, varName,
+        astPath.extend(entry));
   }
 
   public ASTRecord extend(Kind kind, String sel) {
