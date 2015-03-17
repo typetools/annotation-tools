028d08b20367d25bb010c0b00778d43ab97654d3
Dan Brown <dbro@cs.washington.edu>
2015-03-17 13:50:51 -0700

    Adds more test cases for better coverage of inner types.

--- annotation-file-utilities/tests/system-test/source-plain/AnnotationTest.java
+++ annotation-file-utilities/tests/system-test/source-plain/AnnotationTest.java
@@ -3,8 +3,15 @@ package annotations.tests;
 import java.util.*;
 
 public class AnnotationTest<Foo extends Comparable<Integer>> {
+    class Outer {
+        class Inner<Baz> {
+            int baz(Baz o) { return o.hashCode() ^ this.hashCode(); }
+        }
+    }
 
-    Iterable<String> field;
+    Iterable<String[]> field;
+    Outer.Inner<String> inner;
+    Map.Entry<Integer, ? extends CharSequence> entry;
 
     <Bar extends Comparable<Integer>> HashSet<Integer>
         doSomething(Set<Integer> param) {
