54b5c13ca898b49eb373be76e761aee519609d55
Dan Brown <dbro@cs.washington.edu>
2015-07-08 16:43:26 -0700

    Adds class bound test.

--- annotation-file-utilities/tests/ExtImpl.java
+++ annotation-file-utilities/tests/ExtImpl.java
@@ -4,47 +4,48 @@ public class ExtImpl {
   class Top<X, Y> {}
   interface Iface<A, B> {}
   interface Iface2<C, D> {}
-  interface I {}
+  interface Iface3 {}
+  interface Iface4<T, S extends Iface4<T, S>> {}
 
   class C1 extends Top<Object, String> implements Iface<Integer, String> {}
 
   class C2 implements Iface<String, Object>, Iface2<Object, Float> {}
 
   class C3 {
-    class I implements annotator.tests.ExtImpl.I {}
+    class Iface3 implements annotator.tests.ExtImpl.Iface3 {}
 
     /*
      * the jaif file  says that the simple name of
      * the return type in JVM format is
-     * LI;
+     * LIface3;
      */
-    annotator.tests.ExtImpl.C3.I getI1() {
+    annotator.tests.ExtImpl.C3.Iface3 getI1() {
       return null;
     }
 
     /*
      * in this case, the jaif file uses the fully qualified name
      * for the return type
-     * Lannotator.tests.ExtImpl.C3.I;
+     * Lannotator.tests.ExtImpl.C3.Iface3;
      */
-    annotator.tests.ExtImpl.C3.I getI2() {
+    annotator.tests.ExtImpl.C3.Iface3 getI2() {
       return null;
     }
 
     /*
      * the jaif file uses the simple name of the return type
-     * LC3$I;
+     * LC3$Iface3;
      */
-    I getI3() {
+    Iface3 getI3() {
       return null;
     }
 
     /*
-     * in the jaif file, the return type is I
+     * in the jaif file, the return type is Iface3
      * (ambiguous: could be short for the interface
-     * annotator.tests.ExtImpl.I)
+     * annotator.tests.ExtImpl.Iface3)
      */
-    I getI4() {
+    Iface3 getI4() {
       return null;
     }
   }
