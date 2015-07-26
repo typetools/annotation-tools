6a88ccfca635e5beb314120ff5db51ebd9e0f9eb
Werner Dietl <wdietl@gmail.com>
2015-07-25 23:25:30 -0400

    Trying to simplify travis build.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd $HOME
+cd $TRAVIS_BUILD_DIR/..
 git clone https://github.com/typetools/jsr308-langtools.git
 cd jsr308-langtools/make
 ant
 cd $TRAVIS_BUILD_DIR
 pwd
 ant clean
-ant -Dannotations-compiler=$HOME/jsr308-langtools -Dannotations-disassembler=$HOME/jsr308-langtools all
+ant all
