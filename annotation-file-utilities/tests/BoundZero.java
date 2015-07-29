86ae508aa9ef4622c3cc79ee5f97d2596cca4658
Dan Brown <dbro@cs.washington.edu>
2015-07-29 13:14:22 -0700

    Changes expected output to conform to corrected bound zero logic.

--- annotation-file-utilities/tests/BoundZero.java
+++ annotation-file-utilities/tests/BoundZero.java
@@ -1,3 +1,5 @@
+package annotator.tests;
+
 import static java.lang.annotation.ElementType.TYPE_USE;
 import java.lang.annotation.Target;
 
@@ -5,13 +7,11 @@ import java.lang.annotation.Target;
 @Target({TYPE_USE}) @interface Y {}
 
 class BoundZero {
-  <T> void m0(T o) {}
-  <T extends Comparable<T>> void m1(T o) {}
-  <T extends Object & Comparable<T>> void m2(T o) {}
-  <T extends @X Object & Comparable<T>> void m3(T o) {}
-  <T extends @java.lang.X Object & Comparable<T>> void m4(T o) {}
-  <T extends java.lang.Object & Comparable<T>> void m5(T o) {}
-  <T extends java.lang.@X Object & Comparable<T>> void m6(T o) {}
-  <T extends java.lang.@java.lang.X Object & Comparable<T>> void m7(T o) {}
+  <T extends Object> void m1(T o) {}
+  <T extends @Y Object & Comparable<T>> void m2(T o) {}
+  <T extends @annotator.tests.Y Object & Comparable<T>> void m3(T o) {}
+  <T extends java.lang.Object & Comparable<T>> void m4(T o) {}
+  <T extends java.lang.@Y Object & Comparable<T>> void m5(T o) {}
+  <T extends java.lang.@annotator.tests.Y Object & Comparable<T>> void m6(T o) {}
 }
 
