73c535d4d426d2c7146acece6daf3d82a0fa3c46
Werner Dietl <wdietl@gmail.com>
2015-07-28 23:25:43 -0400

    Try different approach to travis-ci setup.

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd $TRAVIS_BUILD_DIR/..
+ROOT=$TRAVIS_BUILD_DIR/..
 git clone https://github.com/typetools/jsr308-langtools.git
 cd jsr308-langtools/
 ./.travis-build.sh
 
-cd $TRAVIS_BUILD_DIR
+cd $ROOT/annotation-tools
 ant clean
 ant all
