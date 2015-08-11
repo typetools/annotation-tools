f72146cd2b0cd280930b9b96e51156e1486065dc
Dan Brown <dbro@cs.washington.edu>
2015-08-11 11:47:56 -0700

    Tests qualified parameterized type fix.

--- annotation-file-utilities/tests/InnerTypeResolution.java
+++ annotation-file-utilities/tests/InnerTypeResolution.java
@@ -6,5 +6,9 @@ public class InnerTypeResolution {
     Map.Entry method01(Map m){
         return null;
     }
+
+    Map.Entry method02(java.util.Map<String, Object> m){
+        return null;
+    }
 }
 
