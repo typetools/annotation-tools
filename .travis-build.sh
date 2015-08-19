7b4626555568ca4953c69dd5a51d1b84186fca7e
Werner Dietl <wdietl@gmail.com>
2015-08-18 23:16:00 -0400

    Attempt to trigger a down-stream project.

--- .travis-build.sh
+++ .travis-build.sh
@@ -8,3 +8,7 @@ cd jsr308-langtools/
 cd $ROOT/annotation-tools
 ant clean
 ant all
+
+curl https://raw.githubusercontent.com/mernst/plume-lib/master/bin/trigger-travis.sh > trigger-travis.sh
+bash trigger-travis.sh typetools checker-framework $TRAVISTOKEN
+rm trigger-travis.sh
