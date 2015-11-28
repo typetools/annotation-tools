b81f1753b37fe53afb3e0e2e92314f674d64dceb
Dan Brown <dbro@cs.washington.edu>
2015-11-27 21:36:58 -0800

    Extends test InheritedMethod with inner type insertions.

--- annotation-file-utilities/tests/InheritedMethod.java
+++ annotation-file-utilities/tests/InheritedMethod.java
@@ -1,5 +1,13 @@
 package annotator.tests;
 
 class InheritedMethod {
+  abstract class Base {
+    public abstract int f();
+    public int[] g(String[] a, int i) { return new int[0]; }
+  }
+
+  public class Super extends Base {
+    public int f() { return 0; }
+  }
 }
 
