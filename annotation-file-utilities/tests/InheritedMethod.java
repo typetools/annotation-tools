e37601e6f56b33e00b7a05e2e6ef9d8060cae5eb
Dan Brown <dbro@cs.washington.edu>
2015-12-04 16:48:06 -0800

    Tests for method overrides with type variables instantiated.

--- annotation-file-utilities/tests/InheritedMethod.java
+++ annotation-file-utilities/tests/InheritedMethod.java
@@ -1,13 +1,12 @@
 package annotator.tests;
 
 class InheritedMethod {
-  abstract class Base {
-    public abstract int f();
+  abstract class Base<T> {
+    public T f(T t) { return t; }
     public int[] g(String[] a, int i) { return new int[0]; }
   }
 
-  public class Sub extends Base {
-    public int f() { return 0; }
+  public class Sub extends Base<Integer> {
   }
 }
 
