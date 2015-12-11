370482106bce585765e46772d89a9025274df9ea
Werner Dietl <wdietl@gmail.com>
2015-12-11 18:14:29 -0500

    Fix build script grammar error.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -2,7 +2,7 @@
 ROOT=$TRAVIS_BUILD_DIR/..
 
 # jsr308-langtools
-(cd $ROOT && hg clone https://bitbucket.org/typetools/jsr308-langtools
+(cd $ROOT && hg clone https://bitbucket.org/typetools/jsr308-langtools)
 (cd $ROOT/jsr308-langtools/ && ./.travis-build-without-test.sh)
 
 ant compile
