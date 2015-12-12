cfbcb07bf32d01066c1ab77de07c569597313a5d
Michael Ernst <mernst@cs.washington.edu>
2015-12-11 18:46:59 -0800

    Don't ignore errors in shell scripts

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 ROOT=$TRAVIS_BUILD_DIR/..
 
+# Fail the whole script if any command fails
+set -e
+
 # jsr308-langtools
 (cd $ROOT && hg clone https://bitbucket.org/typetools/jsr308-langtools)
 (cd $ROOT/jsr308-langtools/ && ./.travis-build-without-test.sh)
