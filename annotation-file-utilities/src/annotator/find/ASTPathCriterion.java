3463189ba80b7055a7e80835a705cb1c2e2aafc3
Paulo Barros <pbsfcin@gmail.com>
2016-04-19 21:41:15 -0300

    Code review feedback.

--- annotation-file-utilities/src/annotator/find/ASTPathCriterion.java
+++ annotation-file-utilities/src/annotator/find/ASTPathCriterion.java
@@ -384,6 +384,8 @@ public class ASTPathCriterion implements Criterion {
                 } else if (astNode.childSelectorIs(ASTPath.BOUND)) {
                   return arg < 0 ? clazz.getExtendsClause()
                           : clazz.getImplementsClause().get(arg);
+                } else {
+                    return null;
                 }
             }
             case CONDITIONAL_EXPRESSION: {
