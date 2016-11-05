61f04c74cb5262953049c3c755e8f050dce7f34e
Werner Dietl <wdietl@gmail.com>
2016-11-04 23:47:43 -0400

    Make up- and down-stream projects stay within the same repository owner.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -6,11 +6,21 @@ set -e
 
 export SHELLOPTS
 
+
+# Split $TRAVIS_REPO_SLUG into the owner and repository parts
+OIFS=$IFS
+IFS='/'
+read -r -a slugarray <<< "$TRAVIS_REPO_SLUG"
+SLUGOWNER=${slugarray[0]}
+SLUGREPO=${slugarray[1]}
+IFS=$OIFS
+
+
 # jsr308-langtools
 if [ -d ../jsr308-langtools ] ; then
     (cd ../jsr308-langtools && hg pull && hg update)
 else
-    (cd .. && (hg clone https://bitbucket.org/typetools/jsr308-langtools || hg clone https://bitbucket.org/typetools/jsr308-langtools))
+    (cd .. && (hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools || hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools))
 fi
 (cd ../jsr308-langtools/ && ./.travis-build-without-test.sh)
 
