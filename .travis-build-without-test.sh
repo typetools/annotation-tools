29448426e1d1c67cda187c526f3abbd2763a997d
Werner Dietl <wdietl@gmail.com>
2016-11-20 17:05:39 -0500

    Make up- and down-stream projects stay within the same repository owner. (#127)
    * Make up- and down-stream projects stay within the same repository owner.
    
    * Use much shorter way to determine repository owner.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -6,11 +6,13 @@ set -e
 
 export SHELLOPTS
 
+SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+
 # jsr308-langtools
 if [ -d ../jsr308-langtools ] ; then
     (cd ../jsr308-langtools && hg pull && hg update)
 else
-    (cd .. && (hg clone https://bitbucket.org/typetools/jsr308-langtools || hg clone https://bitbucket.org/typetools/jsr308-langtools))
+    (cd .. && (hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools || hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools))
 fi
 (cd ../jsr308-langtools/ && ./.travis-build-without-test.sh)
 
