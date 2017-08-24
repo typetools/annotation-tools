242b3cfd7437f5380e95fb79f2c2c4e4bbb3b63d
Werner Dietl <wdietl@gmail.com>
2017-08-24 19:30:50 -0400

    Remove bootclasspath hacks (#138)
    * As we no longer support Java 7, we don't need to change the
    bootclasspath.
    
    * Remove assert that is inconsistent with the documentation.
    This wasn't triggered before, because classes on the bootclasspath don't
    have assertions enabled.
    
    * Fix javadoc error. Errors in javadoc are now failures.
    
    * No longer test on JDK 7.

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,10 +6,9 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities.jar
-:: Contains new version of java.lang.annotation.ElementType.
 set JAVAC_JAR=%~d0
 set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
 set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
 
-java -ea "-Xbootclasspath/p:%ANNOTATION_FILE_UTILS%;%JAVAC_JAR%" -cp "%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotations.io.classfile.ClassFileReader %*
+java -ea -cp "%JAVAC_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotations.io.classfile.ClassFileReader %*
 
