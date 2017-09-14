3fec6f3668a70979aade047e2011484fae5234ed
Michael Ernst <mernst@cs.washington.edu>
2017-09-14 05:06:52 -0700

    Split tests into two jobs

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,17 +1,56 @@
 #!/bin/bash
 
+# Optional argument $1 is one of:
+#   all, test, misc
+# If it is omitted, this script does everything.
+export GROUP=$1
+if [[ "${GROUP}" == "" ]]; then
+  export GROUP=all
+fi
+
+if [[ "${GROUP}" != "all" && "${GROUP}" != "test" && "${GROUP}" != "misc" ]]; then
+  echo "Bad argument '${GROUP}'; should be omitted or one of: all, test, misc."
+  exit 1
+fi
+
 # Fail the whole script if any command fails
 set -e
 
+
+## Diagnostic output
+# Output lines of this script as they are read.
+set -o verbose
+# Output expanded lines of this script as they are executed.
+set -o xtrace
+# Don't use "-d" to debug ant, because that results in a log so long
+# that Travis truncates the log and terminates the job.
+
 export SHELLOPTS
+
+SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
+if [[ "$SLUGOWNER" == "" ]]; then
+  SLUGOWNER=typetools
+fi
 
 ./.travis-build-without-test.sh
 
-ant all
+set -e
 
-ant check-style
+if [[ "${GROUP}" == "test" || "${GROUP}" == "all" ]]; then
+  ant test
+fi
 
-# TODO: when codebase is reformatted (after merging branches?)
-# ant check-format
+if [[ "${GROUP}" == "misc" || "${GROUP}" == "all" ]]; then
+  ## jdkany tests: miscellaneous tests that shouldn't depend on JDK version.
 
-ant html-validate
+  set -e
+
+  ant check-style
+
+  # TODO: when codebase is reformatted (after merging branches?)
+  # ant check-format
+
+  ant html-validate
+
+  ant javadoc
+fi
