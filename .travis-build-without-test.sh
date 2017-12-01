b991a87c0b5be09a77d428ecad2e537e895488f9
Suzanne Millstein <smillst@cs.washington.edu>
2017-12-01 09:13:52 -0800

    Revert "Clone langtools using ssh."
    This reverts commit dde5e400db9a2d2d9a847c8b18d9c2c6050eb9e4.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -16,15 +16,15 @@ if [ -d ../jsr308-langtools ] ; then
     (cd ../jsr308-langtools && hg pull && hg update)
 else
     set +e
-    echo "Running: hg identify ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools &>-"
-    hg identify ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools &>-
+    echo "Running: hg identify https://bitbucket.org/${SLUGOWNER}/jsr308-langtools &>-"
+    hg identify https://bitbucket.org/${SLUGOWNER}/jsr308-langtools &>-
     if [ "$?" -ne 0 ]; then
         SLUGOWNER=typetools
     fi
     set -e
-    echo "Running:  (cd .. && hg clone ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools)"
-    (cd .. && (hg clone ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools || hg clone ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools))
-    echo "... done: (cd .. && hg clone ssh://hg@bitbucket.org/${SLUGOWNER}/jsr308-langtools"
+    echo "Running:  (cd .. && hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools)"
+    (cd .. && (hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools || hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools))
+    echo "... done: (cd .. && hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools)"
 fi
 (cd ../jsr308-langtools/ && ./.travis-build-without-test.sh)
 
