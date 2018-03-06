47797c2c5b3d0d1e83225a79ffffc42e24ff4a7c
Suzanne Millstein <smillst@cs.washington.edu>
2018-03-06 08:14:35 -0800

    Update bat files.

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,9 +6,8 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities.jar
-set JAVAC_JAR=%~d0
-set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
-set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
+set TOOLS_JAR=%JAVA_HOME%%~p0
+set TOOLS_JAR=%TOOLS_JAR%\lib\tools.jar
 
-java -ea -cp "%JAVAC_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" scenelib.annotations.io.classfile.ClassFileReader %*
+java -ea -cp "%TOOLS_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" scenelib.annotations.io.classfile.ClassFileReader %*
 
--- annotation-file-utilities/scripts/insert-annotations-to-source.bat
+++ annotation-file-utilities/scripts/insert-annotations-to-source.bat
@@ -5,8 +5,7 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities.jar
-set JAVAC_JAR=%~d0
-set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
-set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
+set TOOLS_JAR=%JAVA_HOME%%~p0
+set TOOLS_JAR=%TOOLS_JAR%\lib\tools.jar
 
-java -ea -cp "%JAVAC_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotator.Main %*
+java -ea -cp "%TOOLS_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" annotator.Main %*
