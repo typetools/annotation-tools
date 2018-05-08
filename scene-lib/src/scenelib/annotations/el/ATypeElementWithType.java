7e2cee4b73b7172abea01b279c83f76e1d2867d0
Sagar Tewari <sagartewariym@yahoo.com>
2018-05-08 07:27:31 +0530

    Added javadoc comment

--- scene-lib/src/scenelib/annotations/el/ATypeElementWithType.java
+++ scene-lib/src/scenelib/annotations/el/ATypeElementWithType.java
@@ -80,6 +80,11 @@ public class ATypeElementWithType extends ATypeElement {
             && ((ATypeElementWithType) o).equalsTypeElementWithType(this);
     }
 
+    /**
+     * Returns whether this {@link ATypeElementWithType} equals <code>o</code>; a
+     * slightly faster variant of {@link #equals(Object)} for when the argument
+     * is statically known to be another nonnull {@link ATypeElementWithType}.
+     */
     public boolean equalsTypeElementWithType(ATypeElementWithType o) {
         return super.equals(o) && o.type.equals(type);
     }
