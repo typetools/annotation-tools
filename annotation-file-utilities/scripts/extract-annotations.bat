ef786602cbc987211172b1c5481b97848cd1e8ea
Michael Ernst <mernst@cs.washington.edu>
2018-08-15 10:32:23 -0700

    Simplify Windows batch files [ci skip]

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,8 +6,7 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities.jar
-set TOOLS_JAR=%JAVA_HOME%%~p0
-set TOOLS_JAR=%TOOLS_JAR%\lib\tools.jar
+set TOOLS_JAR=%JAVA_HOME%\lib\tools.jar
 
 java -ea -cp "%TOOLS_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" scenelib.annotations.io.classfile.ClassFileReader %*
 
