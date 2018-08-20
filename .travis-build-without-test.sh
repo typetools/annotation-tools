847754d3798232196f076aa5e9fdefc7826fe8c7
Suzanne Millstein <smillst@cs.washington.edu>
2018-08-20 15:26:00 -0700

    Don't use jsr308-langtools repository

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -9,7 +9,6 @@ export SHELLOPTS
 
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
 
-export JSR308=`readlink -f ${JSR308:-..}`
 export AFU=`readlink -f ${AFU:-../annotation-tools/annotation-file-utilities}`
 export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 
@@ -20,24 +19,6 @@ SLUGOWNER=${TRAVIS_REPO_SLUG%/*}
 if [[ "$SLUGOWNER" == "" ]]; then
   SLUGOWNER=typetools
 fi
-
-
-# jsr308-langtools
-if [ -d ../jsr308-langtools ] ; then
-    (cd ../jsr308-langtools && hg pull && hg update)
-else
-    set +e
-    echo "Running: hg identify https://bitbucket.org/${SLUGOWNER}/jsr308-langtools"
-    hg identify https://bitbucket.org/${SLUGOWNER}/jsr308-langtools &>/dev/null
-    if [ "$?" -ne 0 ]; then
-        SLUGOWNER=typetools
-    fi
-    set -e
-    echo "Running:  (cd .. && hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools)"
-    (cd .. && (hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools || hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools))
-    echo "... done: (cd .. && hg clone https://bitbucket.org/${SLUGOWNER}/jsr308-langtools)"
-fi
-(cd ../jsr308-langtools/ && ./.travis-build-without-test.sh)
 
 ## Compile
 echo "running \"ant compile\" for annotation-tools"
--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,9 +6,7 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities-all.jar
-set JAVAC_JAR=%~d0
-set JAVAC_JAR=%ANNOTATION_FILE_UTILS%%~p0
-set JAVAC_JAR=%JAVAC_JAR%..\..\..\jsr308-langtools\dist\lib\javac.jar
+set JAVAC_JAR=%ANNOTATION_FILE_UTILS%\annotation-file-utilities\lib\compiler-2.4.0.jar
 
 java -ea -cp "%JAVAC_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" scenelib.annotations.io.classfile.ClassFileReader %*
 
