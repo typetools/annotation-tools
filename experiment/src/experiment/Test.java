d21b95a6cf411cf8868735cf5dca21b8d0f0db76
Kush Gupta <bluek7@cs.washington.edu>
2019-03-20 05:12:40 -0700

    Small style changes in experiment.Test

--- experiment/src/experiment/Test.java
+++ experiment/src/experiment/Test.java
@@ -1,10 +1,15 @@
 package experiment;
 
-import experiment.Annotations.*;
+import experiment.Annotations.A;
+import experiment.Annotations.B;
+import experiment.Annotations.C;
+import experiment.Annotations.D;
 
-@A public class Test<@B T1 extends Super1> extends @C Super2<@D T2> {
-}
+@A
+public class Test<@B T1 extends Super1> extends @C Super2<@D T2> { }
 
 class Super1 { }
+
 class Super2<E> { }
+
 class T2 { }
