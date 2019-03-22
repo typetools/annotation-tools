831a5d0cfc782b0e5abcef6ae28702b36252a902
Michael Ernst <mernst@cs.washington.edu>
2019-03-22 13:10:09 -0700

    Add documentation

--- scene-lib/src/scenelib/annotations/el/TypeIndexLocation.java
+++ scene-lib/src/scenelib/annotations/el/TypeIndexLocation.java
@@ -2,6 +2,9 @@ package scenelib.annotations.el;
 
 import scenelib.annotations.util.Hasher;
 
+// Gives an index into a class's set of supertypes (-1 = superclass,
+// non-negative integers for implemented interfaces) or into a
+// method's set of thrown exceptions.
 public class TypeIndexLocation {
   public final int typeIndex;
 
