39b6c7cbd628a7149ee1e6152463fed4e4691ec1
Kush Gupta <bluek7@cs.washington.edu>
2019-05-02 22:28:11 -0700

    Added reflection code to LocalLocation to access Label for backwards compatibility with some usages of existing constructor

--- scene-lib/src/scenelib/annotations/el/LocalLocation.java
+++ scene-lib/src/scenelib/annotations/el/LocalLocation.java
@@ -2,12 +2,13 @@ package scenelib.annotations.el;
 
 import org.objectweb.asm.Label;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 
 /**
  * A {@link LocalLocation} holds location information for a local
- * variable: slot index, scope start, and scope length.
+ * variable: slot index, scope start, and scope end.
  */
 public final class LocalLocation {
 
@@ -19,6 +20,38 @@ public final class LocalLocation {
         this.start = start;
         this.end = end;
         this.index = index;
+    }
+
+    public LocalLocation(int index, int scopeStart, int scopeLength) {
+        // Changes values reflectively.
+        this.index = new int[] {index};
+        this.start = new Label[] {new Label()};
+        this.end = new Label[] {new Label()};
+
+        try {
+            Field flagsField = Label.class.getDeclaredField("flags");
+            Field bytecodeOffsetField = Label.class.getDeclaredField("bytecodeOffset");
+            Field FLAG_RESOLVED_FIELD = Label.class.getDeclaredField("FLAG_RESOLVED");
+
+            flagsField.setAccessible(true);
+            bytecodeOffsetField.setAccessible(true);
+            FLAG_RESOLVED_FIELD.setAccessible(true);
+
+            int FLAG_RESOLVED = (Integer) FLAG_RESOLVED_FIELD.get(null);
+
+            short flagsStart = (Short) flagsField.get(start[0]);
+            short flagsEnd = (Short) flagsField.get(end[0]);
+            flagsStart |= FLAG_RESOLVED;
+            flagsEnd |= FLAG_RESOLVED;
+
+            flagsField.set(start[0], flagsStart);
+            bytecodeOffsetField.set(start[0], scopeStart);
+
+            flagsField.set(end[0], flagsEnd);
+            bytecodeOffsetField.set(end[0], scopeStart + scopeLength);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
