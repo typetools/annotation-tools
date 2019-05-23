04221fd3c884ae48ed622009b4b272ab2bc7ac20
Michael Ernst <mernst@cs.washington.edu>
2019-05-23 12:12:41 -0700

    Don't print uses as well as definitions of JDK internal classes

--- scene-lib/src/scenelib/annotations/io/IndexFileWriter.java
+++ scene-lib/src/scenelib/annotations/io/IndexFileWriter.java
@@ -148,8 +148,10 @@ public final class IndexFileWriter {
 
     private void printAnnotations(Collection<? extends Annotation> annos) {
         for (Annotation tla : annos) {
-            pw.print(' ');
-            printAnnotation(tla);
+            if (!tla.def.name.contains("+")) {
+                pw.print(' ');
+                printAnnotation(tla);
+            }
         }
     }
 
