7bbe151c06afdbfbde44cf2fdf2db7aed39b6b0c
Kush Gupta <bluek7@cs.washington.edu>
2019-06-01 20:10:34 -0700

    Added documentation to MethodRecorder.

--- scene-lib/src/scenelib/annotations/util/MethodRecorder.java
+++ scene-lib/src/scenelib/annotations/util/MethodRecorder.java
@@ -7,9 +7,23 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * A MethodRecorder is a {@link ClassVisitor} which simply keeps track of the methods, fields and declaration annotations
+ * inside a class. The main use of this class is to get this information in the order it is visited by {@link ClassWriter},
+ * which is the order in which they occur in the classfile.
+ */
 public class MethodRecorder extends ClassVisitor {
+  /**
+   * Methods of the class.
+   */
   private List<String> methods;
+  /**
+   * Declaration annotations of the class.
+   */
   private List<String> annotations;
+  /**
+   * Fields of the class.
+   */
   private List<String> fields;
 
   public MethodRecorder(int api) {
@@ -35,14 +49,26 @@ public class MethodRecorder extends ClassVisitor {
     return super.visitAnnotation(descriptor, visible);
   }
 
+  /**
+   * Returns the methods in the class in the order that they occur in the classfile.
+   * @return list containing names of methods in the class.
+   */
   public List<String> getMethods() {
     return Collections.unmodifiableList(methods);
   }
 
+  /**
+   * Returns the declaration annotations in the class in the order that they occur in the classfile.
+   * @return list containing names of declaration annotations in the class.
+   */
   public List<String> getAnnotations() {
     return Collections.unmodifiableList(annotations);
   }
 
+  /**
+   * Returns the fields in the class in the order that they occur in the classfile.
+   * @return list containing names of fields in the class.
+   */
   public List<String> getFields() {
     return Collections.unmodifiableList(fields);
   }
