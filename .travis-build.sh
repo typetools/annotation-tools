65813c52688759014624ac07d56578dd83bcc044
Michael Ernst <mernst@cs.washington.edu>
2019-07-10 12:27:02 -0700

    Revert "Debugging"
    This reverts commit d2755ce68e34fe467c19ea4334be7a1b6f034803.

--- .travis-build.sh
+++ .travis-build.sh
@@ -35,7 +35,6 @@ set -e
 
 git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
   || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
-/tmp/plume-scripts/ci-info typetools
 eval `/tmp/plume-scripts/ci-info typetools`
 
 if [[ "${GROUP}" == "test" || "${GROUP}" == "all" ]]; then
