74e68d015705ad32c9e44b35e4a5cd8943af15f8
Michael Ernst <mernst@cs.washington.edu>
2019-07-11 07:42:09 -0700

    Tweaks

--- annotation-file-utilities/dev-scripts/test-downstream.sh
+++ annotation-file-utilities/dev-scripts/test-downstream.sh
@@ -6,7 +6,8 @@ set -o xtrace
 export SHELLOPTS
 
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
-export AFU=`readlink -f ${AFU:-$(dirname $0)/..}`
+
+export AFU=`readlink -f ${AFU:-../annotation-tools/annotation-file-utilities}`
 export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
