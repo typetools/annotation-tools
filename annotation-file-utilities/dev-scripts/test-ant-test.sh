8fd2c08117cb279f1d69f3a631ab031885f7326c
Michael Ernst <mernst@cs.washington.edu>
2019-07-11 07:18:31 -0700

    Clean up path setting

--- annotation-file-utilities/dev-scripts/test-ant-test.sh
+++ annotation-file-utilities/dev-scripts/test-ant-test.sh
@@ -6,7 +6,7 @@ set -o xtrace
 export SHELLOPTS
 
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
-export AFU=`readlink -f ${AFU:-../annotation-tools/annotation-file-utilities}`
+export AFU=`readlink -f ${AFU:-$(dirname $0)/..}`
 export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
