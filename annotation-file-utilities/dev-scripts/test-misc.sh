6fc99daa3386b2916b496648b31a3cbb0d05aff5
Michael Ernst <mernst@cs.washington.edu>
2019-07-11 06:39:54 -0700

    Move scripts to subdirectory

--- annotation-file-utilities/dev-scripts/test-misc.sh
+++ annotation-file-utilities/dev-scripts/test-misc.sh
@@ -6,7 +6,7 @@ set -o xtrace
 export SHELLOPTS
 
 export JAVA_HOME=${JAVA_HOME:-$(dirname $(dirname $(dirname $(readlink -f $(/usr/bin/which java)))))}
-export PATH=$(dirname $0)/scripts:$JAVA_HOME/bin:$PATH
+export PATH=$(dirname $0)/../scripts:$JAVA_HOME/bin:$PATH
 
 ant compile
 
