81f31818ac605d28e18bea0e0707f6af8aa441a2
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-14 11:28:36 -0700

    Don't build langtools.

--- .travis-build-without-test.sh
+++ .travis-build-without-test.sh
@@ -14,22 +14,6 @@ export CHECKERFRAMEWORK=`readlink -f ${CHECKERFRAMEWORK:-../checker-framework}`
 
 export PATH=$AFU/scripts:$JAVA_HOME/bin:$PATH
 
-git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
-    || git -C /tmp clone --depth 1 -q https://github.com/plume-lib/plume-scripts.git
-eval `/tmp/plume-scripts/ci-info eisop`
-
-# jsr308-langtools
-if [ -d ../jsr308-langtools ] ; then
-    (cd ../jsr308-langtools && hg pull && hg update)
-else
-    CI_ORGANIZATION=eisop
-    echo "Running:  (cd .. && hg clone https://bitbucket.org/${CI_ORGANIZATION}/jsr308-langtools)"
-    (cd .. && (hg clone https://bitbucket.org/${CI_ORGANIZATION}/jsr308-langtools || hg clone https://bitbucket.org/${CI_ORGANIZATION}/jsr308-langtools))
-    echo "... done: (cd .. && hg clone https://bitbucket.org/${CI_ORGANIZATION}/jsr308-langtools)"
-fi
-(cd ../jsr308-langtools/ && ./.travis-build-without-test.sh)
-
-
 ## Compile
 echo "About to run \"ant compile\" for annotation-tools" in `pwd`
 which ant
