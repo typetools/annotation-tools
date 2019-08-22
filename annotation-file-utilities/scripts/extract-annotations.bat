fbb8a65da565253786a451eb951bf30407e9473f
Suzanne Millstein <smillst@cs.washington.edu>
2019-08-22 11:06:58 -0700

    Use latest error prone javac.

--- annotation-file-utilities/scripts/extract-annotations.bat
+++ annotation-file-utilities/scripts/extract-annotations.bat
@@ -6,7 +6,7 @@
 set ANNOTATION_FILE_UTILS=%~d0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%%~p0
 set ANNOTATION_FILE_UTILS=%ANNOTATION_FILE_UTILS%\..\annotation-file-utilities-all.jar
-set JAVAC_JAR=%ANNOTATION_FILE_UTILS%\annotation-file-utilities\lib\javac-9-dev-r4023-3.jar
+set JAVAC_JAR=%ANNOTATION_FILE_UTILS%\annotation-file-utilities\lib\javac-9+181-r4173-1.jar
 
 java -ea -cp "%JAVAC_JAR%;%ANNOTATION_FILE_UTILS%;%CLASSPATH%" scenelib.annotations.io.classfile.ClassFileReader %*
 
