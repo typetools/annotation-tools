e8120dd3d52a3fa7b8686e47cc863da471595c78
Kush Gupta <bluek7@cs.washington.edu>
2019-09-08 11:25:24 -0700

    Removed experiments

--- experiment/src/experiment/Test.java
+++
@@ -1,9 +0,0 @@
-package experiment;
-
-import experiment.Annotations.A;
-import experiment.Annotations.B;
-import experiment.Annotations.C;
-import experiment.Annotations.D;
-
-@A
-public class Test<@B T1 extends Super1> extends @C Super2<@D T2> { }
--- experiment/src/experiment/TestBig.java
+++
@@ -1,5 +0,0 @@
-package experiment;
-
-import experiment.Annotations.*;
-
-@A class TestBig<@B T1 extends @C Super2<@D T3<@E T2>>> extends @F Super3<@G Super4> { }
