0b4d34c966a65eeccd54c4b3bf49ee41c644d7ac
Michael Ernst <mernst@cs.washington.edu>
2019-09-09 10:44:19 -0700

    Add vertical whitespace

--- annotation-file-utilities/dev-scripts/test-downstream.sh
+++ annotation-file-utilities/dev-scripts/test-downstream.sh
@@ -19,8 +19,10 @@ git -C /tmp/plume-scripts pull > /dev/null 2>&1 \
 # checker-framework and its downstream tests
 /tmp/plume-scripts/git-clone-related typetools checker-framework
 (cd ${CHECKERFRAMEWORK} && checker/bin-devel/build.sh)
+
 (cd ${CHECKERFRAMEWORK}/framework && ../gradlew wholeProgramInferenceTests)
 
 /tmp/plume-scripts/git-clone-related typetools checker-framework-inference
 (cd ../checker-framework-inference && . ./.travis-build-without-test.sh)
+
 (cd ../checker-framework-inference && ./gradlew dist && ./gradlew test)
