3554973177aa5800b9c7709bc92d60f2e2e2c00f
Martin Kellogg <kelloggm@cs.washington.edu>
2019-12-16 15:13:02 -0800

    print longs into .jaif files correctly

--- scene-lib/src/scenelib/annotations/io/IndexFileWriter.java
+++ scene-lib/src/scenelib/annotations/io/IndexFileWriter.java
@@ -123,6 +123,8 @@ public final class IndexFileWriter {
             pw.print(aft.format(o));
         } else if (aft instanceof BasicAFT && o instanceof String) {
             pw.print(Strings.escape((String) o));
+        } else if (aft instanceof BasicAFT && o instanceof Long) {
+            pw.print(o.toString() + "L");
         } else {
             pw.print(o.toString());
         }
