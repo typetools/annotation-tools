73b1ad30569f187f396b01d6ba0feb141ef6cfd1
Michael Ernst <mernst@cs.washington.edu>
2020-04-12 06:16:34 -0700

    Add javadoc

--- scene-lib/src/scenelib/annotations/el/AElement.java
+++ scene-lib/src/scenelib/annotations/el/AElement.java
@@ -48,6 +48,12 @@ public class AElement implements Cloneable {
      */
     public Object description;
 
+    /**
+     * Create a new element with the given description.
+     *
+     * @param description the description of the element, used for debugging and diagnostic
+     *     messages.
+     */
     AElement(Object description) {
         this(description, false);
     }
