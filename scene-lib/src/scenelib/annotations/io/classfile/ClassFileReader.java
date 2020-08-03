7269c32397018ad78a3e0b9775857e40718c00bc
Suzanne Millstein <smillst@cs.washington.edu>
2020-08-03 09:16:30 -0700

    Update for release.

--- scene-lib/src/scenelib/annotations/io/classfile/ClassFileReader.java
+++ scene-lib/src/scenelib/annotations/io/classfile/ClassFileReader.java
@@ -17,7 +17,7 @@ import scenelib.annotations.io.IndexFileWriter;
  *  from a class file into an {@link scenelib.annotations.el.AScene}.
  */
 public class ClassFileReader {
-  public static final String INDEX_UTILS_VERSION = "Annotation File Utilities v3.9.10";
+  public static final String INDEX_UTILS_VERSION = "Annotation File Utilities v3.9.13";
 
   @Option("-b omit annotations from bridge (compiler-created) methods")
   public static boolean ignore_bridge_methods = false;
