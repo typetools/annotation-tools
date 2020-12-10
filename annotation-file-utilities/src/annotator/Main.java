a19f34f2c95571e3d3cbd6347df726c62a2cb237
Michael Ernst <mernst@cs.washington.edu>
2020-12-10 13:27:47 -0800

    Improve usage

--- annotation-file-utilities/src/annotator/Main.java
+++ annotation-file-utilities/src/annotator/Main.java
@@ -513,7 +513,7 @@ public class Main {
     }
 
     Options options = new Options(
-        "Main [options] { jaif-file | java-file | @arg-file } ...\n"
+        "java annotator.Main [options] { jaif-file | java-file | @arg-file } ...\n"
             + "(Contents of argfiles are expanded into the argument list.)",
         Main.class);
     String[] cl_args;
