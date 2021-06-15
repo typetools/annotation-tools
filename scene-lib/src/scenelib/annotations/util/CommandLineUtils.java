b5651989fdf36d200bcba91e493334601509be56
Neil Brown <neil@twistedsquare.com>
2021-06-15 08:38:30 +0100

    Simplified getDeclaredMethod down to getMethod, since the methods are public.

--- scene-lib/src/scenelib/annotations/util/CommandLineUtils.java
+++ scene-lib/src/scenelib/annotations/util/CommandLineUtils.java
@@ -18,10 +18,10 @@ public class CommandLineUtils {
   public static String[] parseCommandLine(String[] args) {
     try {
       try {
-        Method method = CommandLine.class.getDeclaredMethod("parse", List.class);
+        Method method = CommandLine.class.getMethod("parse", List.class);
         return ((List<?>) method.invoke(null, Arrays.asList(args))).toArray(new String[0]);
       } catch (NoSuchMethodException e) {
-        Method method = CommandLine.class.getDeclaredMethod("parse", String[].class);
+        Method method = CommandLine.class.getMethod("parse", String[].class);
         return (String[]) method.invoke(null, (Object) args);
       }
     } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
