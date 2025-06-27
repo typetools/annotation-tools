f8743f0279fd372240c6fdcc7c0432325f8e86fc
Liam Miller-Cushon <cushon@google.com>
2025-06-27 13:05:39 -0700

    Make InitBlockScanner compatible with an upcoming javac change (#719)
    Co-authored-by: Suzanne Millstein <smillst@cs.washington.edu>

--- annotation-file-utilities/src/main/java/org/checkerframework/afu/annotator/scanner/InitBlockScanner.java
+++ annotation-file-utilities/src/main/java/org/checkerframework/afu/annotator/scanner/InitBlockScanner.java
@@ -5,6 +5,7 @@ import com.sun.source.tree.Tree;
 import com.sun.source.util.TreePath;
 import com.sun.source.util.TreePathScanner;
 import com.sun.tools.javac.tree.JCTree.JCBlock;
+import java.lang.reflect.Field;
 
 /**
  * InitScanner scans the source tree and determines the index of a given initializer block, where
@@ -41,12 +42,47 @@ public class InitBlockScanner extends TreePathScanner<Void, Boolean> {
   @Override
   public Void visitBlock(BlockTree node, Boolean isStatic) {
     // TODO: is isStatic only used for static initializer blocks?
-    if (!done && isStatic == node.isStatic() && ((JCBlock) node).endpos >= 0) {
+    if (!done && isStatic == node.isStatic() && getBlockEndPos((JCBlock) node) >= 0) {
       index++;
     }
     if (tree == node) {
       done = true;
     }
     return super.visitBlock(node, isStatic);
+  }
+
+  /**
+   * Returns the end pos for {@code block}.
+   *
+   * @param block a block
+   * @return the end pos for {@code block}
+   */
+  private static int getBlockEndPos(JCBlock block) {
+    try {
+      return BLOCK_END_POS_FIELD.getInt(block);
+    } catch (IllegalAccessException e) {
+      throw new AssertionError(e);
+    }
+  }
+
+  /** Field for JCBlock#endpos or JCBlock#bracePos depending on the version of Java. */
+  private static final Field BLOCK_END_POS_FIELD = getBlockEndPosField();
+
+  /**
+   * Returns field for JCBlock#endpos or JCBlock#bracePos depending on the version of Java.
+   *
+   * @return field for JCBlock#endpos or JCBlock#bracePos depending on the version of Java
+   */
+  private static Field getBlockEndPosField() {
+    try {
+      return JCBlock.class.getDeclaredField("endpos");
+    } catch (NoSuchFieldException e1) {
+      try {
+        return JCBlock.class.getDeclaredField("bracePos");
+      } catch (NoSuchFieldException e2) {
+        e2.addSuppressed(e1);
+        throw new AssertionError(e2);
+      }
+    }
   }
 }
